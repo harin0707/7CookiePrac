78777a940ed9641a86a464cb1c43f5fc44668e1a
harin0707 <yuharin0707@gmail.com>
2022-11-02 16:23:00 +0900

    이미지 2개 이상 중첩

--- src/Prac.js
+++ src/Prac.js
@@ -3,6 +3,9 @@ import styled from 'styled-components'
 import base from './img/base.png'
 import cloth01 from './img/cloth_01.png'
 import cloth02 from './img/cloth_02.png'
+import eyes01 from './img/eyes_01.png'
+import eyes02 from './img/eyes_02.png'
+import hair01 from './img/hair_01.png'
 //img -> json으로 정리
 
 const Container = styled.div`
@@ -49,26 +52,48 @@ border: solid black 0.1vw;
 `
 
 const ImgContainer=styled.div`
-border: solid black;
+display: flex;
+justify-content: center;
+align-items: center;
+
+border: solid green;
 overflow: hidden;
 position: relative;
 `
 
 const ImgCont = styled.div`
 position: absolute;
+
+display: flex;
+justify-content: center;
+align-items: center;
+
+border: solid 1vw red;
 `
+
 const BaseImg = styled.img`
 border: solid black;
 z-index: 0;
+
 `
 
-const Img = styled.img`
+const TopImg = styled.img`
 border: solid black;
 z-index: 1;
-position: relative;
-`
-const ImgItem = styled.div`
 /* position: relative; */
+border: yellow solid 1vw;
+`
+
+const Eyes = styled.img`
+/* position: relative; */
+border: blue solid 2vw;
+position: absolute;
+`
+
+const Hair = styled.img`
+/* position: relative; */
+border: blue solid 2vw;
+position: absolute;
 `
 
 
@@ -77,8 +102,6 @@ const Modal = styled.div`
 `
 
 const Prac = () => {
-    const [item, setItem] = useState('')
-
     //아이템 선택 모달창
     const [showModal, setModal] = useState(false);
     const openModal = () =>{
@@ -89,11 +112,18 @@ const Prac = () => {
         setModal(false);
     }
 
-    const ItemOn =(e) => {
-        setItem(e.currentTarget.getAttribute('value'))
-    }
-    console.log(item)
+    //선택한 아이템 이미지 속성 전달해주는 useState
+    const [top, setTop] = useState('');
+    const [itemVisible, setItemVisible] = useState(false);
 
+
+    const ItemOn =(e) => {
+        setItemVisible(true);
+        setTop(e.currentTarget.getAttribute('value'))
+
+        // setImgUrl(`./img/${top}.png`)
+    }
+    console.log(`./img/${top}.png`)
 
     return (
         <>
@@ -101,14 +131,17 @@ const Prac = () => {
             <Space>옷입히기 공간</Space>
 
             <ImgContainer>
-            <ImgCont>
-            <BaseImg src={base} alt='base'></BaseImg>
-            </ImgCont>
+                <ImgCont>
+                    <BaseImg src={base} alt='base'></BaseImg>
+                </ImgCont>
+
+                <TopImg src={cloth01} alt='상의' visible={itemVisible}></TopImg>
+
+                {/* <TopImg src={cloth02} alt='상의' visible={itemVisible}></TopImg> */}
+                <Eyes src={eyes01} alt='눈' visible={itemVisible}></Eyes>
+                <Hair src={hair01} alt='머리' visible={itemVisible}></Hair>
             
-            {/* <ImgItem> */}
-            <Img src={cloth01} alt='top'></Img>
-            {/* <Img src={cloth02} alt='top'></Img>
-            </ImgItem> */}
+            
             
             </ImgContainer>
             
@@ -126,8 +159,8 @@ const Prac = () => {
                 <>
                 <ModalContainer>
                     <OpenModal> 
-                        <Item src={cloth01} alt='상의1' value='cloth01' onClick={ItemOn}></Item>
-                        <Item src={cloth02} alt='상의2' value='cloth02' onClick={ItemOn}></Item>
+                        <Item src={cloth01} alt='상의1' value='cloth_01' onClick={ItemOn}></Item>
+                        <Item src={cloth02} alt='상의2' value='cloth_02' onClick={ItemOn}></Item>
                         
                     </OpenModal>
                     
