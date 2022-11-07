1b81f2cb78c9b595dc9a399e9058a1daab8c34a5
harin0707 <yuharin0707@gmail.com>
2022-11-07 21:12:58 +0900

    refactor: Image tranform 눈사람 이미지로 변경

--- src/Prac.js
+++ src/Prac.js
@@ -3,11 +3,25 @@ import styled from 'styled-components'
 import base from './img/base.png'
 import cloth01 from './img/cloth_01.png'
 import cloth02 from './img/cloth_02.png'
-import eyes01 from './img/eyes_01.png'
-import eyes02 from './img/eyes_02.png'
+// import eyes01 from './img/eyes_01.png'
+// import eyes02 from './img/eyes_02.png'
 import hair01 from './img/hair_01.png'
 import {useRecoilState} from 'recoil'
-import {itemTopOn, itemEyesOn} from './recoil'
+import {itemTopOn, itemEyesOn, baseEyes, baseArms, baseHead, baseNose, baseItem, baseMouth} from './recoil'
+
+import baseSnowman from './img/민둥눈사람.png'
+import eyes01 from './img/눈1.png'
+import eyes02 from './img/눈2.png'
+import arms01 from './img/팔1.png'
+import arms02 from './img/팔2.png'
+import item01 from './img/소품1.png'
+import item02 from './img/소품2.png'
+import nose01 from './img/코1.png'
+import nose02 from './img/코2.png'
+import head01 from './img/머리1.png'
+import head02 from './img/머리2.png'
+import mouth01 from './img/입1.png'
+import mouth02 from './img/입2.png'
 
 const Container = styled.div`
 display: flex;
@@ -55,6 +69,9 @@ flex-direction: column;
 
 const Item = styled.img`
 border: solid black 0.1vw;
+
+width: 50%;
+height: 50%;
 `
 
 const ImgContainer=styled.div`
@@ -81,68 +98,128 @@ border: solid 1vw red;
 const BaseImg = styled.img`
 border: solid black;
 z-index: 0;
+
+width: 30vw;
+height: 30vw;
 `
 
-const TopImg = styled.img`
+const Eyes = styled.img`
 border: solid black;
 z-index: 1;
 border: yellow solid 1vw;
-`
-const Eyes = styled.img`
-border: blue solid 2vw;
-position: absolute;
+
+width: 30vw;
+height: 30vw;
 `
 
-const Hair = styled.img`
+const Nose = styled.img`
 border: blue solid 2vw;
 position: absolute;
+
+width: 30vw;
+height: 30vw;
 `
+
+const Head = styled.img`
+border: blue solid 2vw;
+position: absolute;
+
+width: 30vw;
+height: 30vw;
+`
+
+const ItemS = styled.img`
+border: blue solid 2vw;
+position: absolute;
+
+width: 30vw;
+height: 30vw;
+`
+
+const Arms = styled.img`
+border: blue solid 2vw;
+position: absolute;
+
+width: 30vw;
+height: 30vw;
+`
+
+const Mouth = styled.img`
+border: blue solid 2vw;
+position: absolute;
+
+width: 30vw;
+height: 30vw;
+`
+
+
 
 const SaveButton = styled.button`
 `
 
-const Modal = ({showModal, modalType, topRef, eyesRef}) =>{
-    const [topR, setTopR] = useRecoilState(itemTopOn);
-    const [eyesR, setEyesR] = useRecoilState(itemEyesOn);
+const Modal = ({showModal, modalType}) =>{
+    // const [topR, setTopR] = useRecoilState(itemTopOn);
+    const [eyes, setEyes] = useRecoilState(baseEyes);
+    const [nose, setNose] = useRecoilState(baseNose);
+    const [arms, setArms] = useRecoilState(baseArms);
+    const [item, setItem] = useRecoilState(baseItem);
+    const [mouth, setMouth] = useRecoilState(baseMouth);
+    const [head, setHead] = useRecoilState(baseHead);
 
-    const handleChangeTop = (e) => {
-        const topItem = e.currentTarget.getAttribute('src');
-        const Image = {topItem};
-        setTopR(Image);
+    //1. 눈
+    const handleChangeEyes = (e) => {
+        const baseEyes = e.currentTarget.getAttribute('src');
+        const Image = {baseEyes};
+        setEyes(Image);
     }
     // console.log(modalType)
-    // console.log(topR.topItem);
+    // console.log(eyes.baseEyes);
 
-    const handleChangeEyes = (e) => {
-        const eyesItem = e.currentTarget.getAttribute('src');
-        const Image = {eyesItem};
-        setEyesR(Image);
+    //2. 코
+    const handleChangeNose = (e) => {
+        const baseNose = e.currentTarget.getAttribute('src');
+        const Image = {baseNose};
+        setNose(Image);
     }
-    // console.log(eyesR.eyesItem)
-    // console.log(topR.topItem);
+    // console.log(Nose.baseNose)
+
+    //3. 팔
+    const handleChangeArms = (e) => {
+        const baseArms = e.currentTarget.getAttribute('src');
+        const Image = {baseArms};
+        setArms(Image);
+    }
+
+    //4. 입
+    const handleChangeMouth = (e) => {
+        const baseMouth = e.currentTarget.getAttribute('src');
+        const Image = {baseMouth};
+        setMouth(Image);
+    }
+
+    //5. 소품
+    const handleChangeItem = (e) => {
+        const baseItem = e.currentTarget.getAttribute('src');
+        const Image = {baseItem};
+        setItem(Image);
+    }
+
+    //6. 머리
+    const handleChangeHead = (e) => {
+        const baseHead = e.currentTarget.getAttribute('src');
+        const Image = {baseHead};
+        setHead(Image);
+    }
+
 
 
     switch (modalType){
-        case 'top' :
-            return(
-                <>
-                {showModal===true ? 
-                        <>
-                        <ModalContainer>
-                            <OpenModal> 
-                                <Item src={cloth01} alt='상의1' value='cloth_01' onClick={handleChangeTop}></Item>
-                                <Item src={cloth02} alt='상의2' value='cloth_02' onClick={handleChangeTop}></Item>
-                            </OpenModal>
-                            <SaveButton> save </SaveButton>
-                        </ModalContainer>
-                        </>
-                    : null}
-                    </>)
         case 'eyes' :
         return(
+            <>
+                    {showModal===true ? 
                 <>
-                {showModal===true ? 
-                        <>
+
                         <ModalContainer>
                             <OpenModal> 
                                 <Item src={eyes01} alt='눈1' value='eyes_01' onClick={handleChangeEyes} ></Item>
@@ -150,10 +227,83 @@ const Modal = ({showModal, modalType, topRef, eyesRef}) =>{
                             </OpenModal> 
                             <SaveButton> save </SaveButton>
                         </ModalContainer>
-                        </>
-                    : null}
-                    </>
+                        </>: null }</>
             )
+
+        case 'nose' :
+            return(
+                <>
+                    {showModal===true ? 
+                    <>
+                            <ModalContainer>
+                                <OpenModal> 
+                                    <Item src={nose01} alt='코1' onClick={handleChangeNose} ></Item>
+                                    <Item src={nose02} alt='코2' onClick={handleChangeNose} ></Item>
+                                </OpenModal> 
+                                <SaveButton> save </SaveButton>
+                            </ModalContainer>
+                            </>:null}</>
+        )
+        case 'head' :
+            return(
+                    <>
+                    {showModal===true ? 
+                            <>
+                            <ModalContainer>
+                                <OpenModal> 
+                                    <Item src={head01} alt='머리1' onClick={handleChangeHead} ></Item>
+                                    <Item src={head02} alt='머리2' onClick={handleChangeHead} ></Item>
+                                </OpenModal> 
+                                <SaveButton> save </SaveButton>
+                            </ModalContainer>
+                            </>
+                        : null}
+                        </>)
+        case 'item' :
+            return(
+                    <>
+                    {showModal===true ? 
+                            <>
+                            <ModalContainer>
+                                <OpenModal> 
+                                    <Item src={item01} alt='소품1' onClick={handleChangeItem} ></Item>
+                                    <Item src={item02} alt='소품2' onClick={handleChangeItem} ></Item>
+                                </OpenModal> 
+                                <SaveButton> save </SaveButton>
+                            </ModalContainer>
+                            </>
+                        : null}
+                        </>)
+        case 'arms' :
+            return(
+                    <>
+                    {showModal===true ? 
+                            <>
+                            <ModalContainer>
+                                <OpenModal> 
+                                    <Item src={arms01} alt='팔1' onClick={handleChangeArms} ></Item>
+                                    <Item src={arms02} alt='팔2' onClick={handleChangeArms} ></Item>
+                                </OpenModal> 
+                                <SaveButton> save </SaveButton>
+                            </ModalContainer>
+                            </>
+                        : null}
+                        </>)
+        case 'mouth' :
+            return(
+                    <>
+                    {showModal===true ? 
+                            <>
+                            <ModalContainer>
+                                <OpenModal> 
+                                    <Item src={mouth01} alt='입1' onClick={handleChangeMouth} ></Item>
+                                    <Item src={mouth02} alt='입2' onClick={handleChangeMouth} ></Item>
+                                </OpenModal> 
+                                <SaveButton> save </SaveButton>
+                            </ModalContainer>
+                            </>
+                        : null}
+                        </>)
         default : return (null)
         
 
@@ -194,6 +344,14 @@ const Prac = () => {
     const [topR, setTopR] = useRecoilState(itemTopOn);
     const [eyesR, setEyesR] = useRecoilState(itemEyesOn);
 
+    const [eyes, setEyes] = useRecoilState(baseEyes);
+    const [nose, setNose] = useRecoilState(baseNose);
+    const [arms, setArms] = useRecoilState(baseArms);
+    const [item, setItem] = useRecoilState(baseItem);
+    const [mouth, setMouth] = useRecoilState(baseMouth);
+    const [head, setHead] = useRecoilState(baseHead);
+
+
 
     return (
         <>
@@ -202,24 +360,32 @@ const Prac = () => {
 
             <ImgContainer>
                 <ImgCont>
-                    <BaseImg src={base} alt='base'></BaseImg>
+                    <BaseImg src={baseSnowman} alt='base'></BaseImg>
                 </ImgCont>
 
-                <TopImg src={topR.topItem} alt='상의' visible={itemVisible} ></TopImg>
-                <Eyes src={eyesR.eyesItem} alt='눈' visible={itemVisible} ></Eyes>
-                <Hair src={hair01} alt='머리' visible={itemVisible} ></Hair>
+                {/* <TopImg src={topR.topItem} alt='상의' visible={itemVisible} ></TopImg> */}
+                <Eyes src={eyes.baseEyes} alt='눈' visible={itemVisible} ></Eyes>
+                <Head src={head.baseHead} alt='머리' visible={itemVisible} ></Head>
+                <Arms src={arms.baseArms} alt='팔' visible={itemVisible} ></Arms>
+                <Mouth src={mouth.baseMouth} alt='입' visible={itemVisible} ></Mouth>
+                <ItemS src={item.baseItem} alt='소품' visible={itemVisible} ></ItemS>
+                <Nose src={nose.baseNose} alt='코' visible={itemVisible} ></Nose>
     
             </ImgContainer>
             
         </Container>
 
         <ButContainer>
-            <Button type='top' onClick={openModal} modal='top'> 상의 </Button>
+            {/* <Button type='top' onClick={openModal} modal='top'> 상의 </Button> */}
             <Button type='eyes' onClick={openModal} modal='eyes'> 눈 </Button>
-            <Button type='shoe' onClick={openModal}> 신발 </Button>
+            <Button type='nose' onClick={openModal} modal='nose'> 코 </Button>
+            <Button type='arms' onClick={openModal} modal='arms'> 팔 </Button>
+            <Button type='item' onClick={openModal} modal='item'> 소품 </Button>
+            <Button type='head' onClick={openModal} modal='head'> 머리 </Button>
+            <Button type='mouth' onClick={openModal} modal='mouth'> 입 </Button>
         </ButContainer>
 
-        <Modal showModal={showModal} close={closeModal} modalType={modalType} />
+        <Modal showModal={showModal} modalType={modalType}/>
 
 
         
--- src/recoil.js
+++ src/recoil.js
@@ -1,6 +1,7 @@
-import {atom, selector } from 'recoil'
+import {atom} from 'recoil'
 import cloth01 from './img/cloth_01.png'
 import eyes01 from './img/eyes_01.png'
+import n_base from './img/투명.png'
 
     //Recoil 사용해보기
 export const itemTopOn = atom({
@@ -8,7 +9,6 @@ export const itemTopOn = atom({
         default: {
             //deault 이미지 첨부
             'topItem' : cloth01,
-            'eyesItem' : eyes01,
         }
     })
 
@@ -19,6 +19,51 @@ export const itemEyesOn = atom({
             'eyesItem' : eyes01,
         }
     })
+
+
+//눈사람 이미지
+export const baseEyes = atom({
+    key: 'baseSnowman_e',
+    default: {
+        'baseEyes' : n_base,
+    }
+})
+
+export const baseNose = atom({
+    key: 'baseSnowman_n',
+    default: {
+        'baseNose' : n_base,
+    }
+})
+
+export const baseArms = atom({
+    key: 'baseSnowman_a',
+    default: {
+        'baseArms' : n_base,
+    }
+})
+
+export const baseItem = atom({
+    key: 'baseSnowman_i',
+    default: {
+        'baseItem' : n_base,
+    }
+})
+
+export const baseHead = atom({
+    key: 'baseSnowman_h',
+    default: {
+        'baseHead' : n_base,
+    }
+})
+
+export const baseMouth = atom({
+    key: 'baseSnowman_m',
+    default: {
+        'baseMouth' : n_base,
+    }
+})
+
 
 // export const getItemImage = selector({
 //         key: "get/itemImage",
